cb21a1072f0782ece87cf96050626f5d56c0a287
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-03-13 22:14:21 -0500

    Remove TODO

--- src/storage_engine/data_store.rs
+++ src/storage_engine/data_store.rs
@@ -9,7 +9,6 @@ use std::convert::From;
 use std::fs::{File, OpenOptions};
 use std::io::{BufWriter, Read, Result, Seek, SeekFrom, Write};
 use std::path::{Path, PathBuf};
-// TODO: Add feature flag to support using `tokio`'s `Mutex` (etc.) instead of the std lib
 use std::sync::{Arc, Mutex, RwLock};
 
 use std::sync::atomic::{AtomicU64, Ordering};
