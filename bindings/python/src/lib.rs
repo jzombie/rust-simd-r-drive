ff6a40bcf8a12ac1305e83f5a56057c60d49ea3e
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-05-22 15:23:08 -0600

    Expose `NamespaceHasher` to Python

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -7,12 +7,15 @@ mod entry_stream;
 use entry_stream::EntryStream;
 mod data_store;
 use data_store::DataStore;
+mod namespace_hasher;
+use namespace_hasher::NamespaceHasher;
 
 #[pymodule(name = "simd_r_drive")]
 fn python_entry(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<DataStore>()?;
     m.add_class::<EntryHandle>()?;
     m.add_class::<EntryStream>()?;
+    m.add_class::<NamespaceHasher>()?;
 
     Ok(())
 }
