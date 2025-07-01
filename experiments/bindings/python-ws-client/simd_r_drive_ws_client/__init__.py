4c5f4c4c2184845028725dbe3595be2c4ad68923
Jeremy Harris <jeremy.harris@zenosmosis.com>
2025-06-30 20:54:33 -0600

    Fix incorrect inheritance for Python WebSocket docs (#64)

--- experiments/bindings/python-ws-client/simd_r_drive_ws_client/__init__.py
+++ experiments/bindings/python-ws-client/simd_r_drive_ws_client/__init__.py
@@ -1,10 +1,9 @@
 from .simd_r_drive_ws_client import (
-    NamespaceHasher,
     setup_logging,
     test_rust_logging,
 )
 
-from .data_store_ws_client import DataStoreWsClient
+from .data_store_ws_client import DataStoreWsClient, NamespaceHasher
 
 __all__ = [
     "DataStoreWsClient",
